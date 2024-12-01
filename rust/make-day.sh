3155e1068ecb91d4e8d1c27d7207e5f37af72360
Jonathan Rothberg <jon@join.build>
2024-11-30 20:21:23 -0800

    Initial commit

---
+++ rust/make-day.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ $# != 1 ]; then
+	echo "Usage: $(basename "$0") <day-number>" >&2
+	exit 1
+fi
+# if [ ! -d .git ]; then
+#     echo "must be run from root of advent-of-code repository" >&2
+#     exit 1
+# fi
+
+name="$(printf "aoc%02d" "$1")"
+cargo new --bin "$name"
+mkdir "$name/input"
+touch "$name/input/sample.txt"
+touch "$name/input/input.txt"
